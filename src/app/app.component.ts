cf662b612dc8daf443a2576782718f20e5042e24
Johnny Gérard <john@jgerard.dev>
2024-05-29 00:40:41 +0200

    feat: reset root component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styles: [],
+  imports: [],
+  templateUrl: "./app.component.html",
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent {
-  title = 'fem-order-summary-component';
-}
+export class AppComponent {}
