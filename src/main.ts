65781b6d58b37fabf6bbde21c8292a55f61f8cb6
Johnny Gérard <john@jgerard.dev>
2024-05-29 00:40:44 +0200

    feat: add console logs for production

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
+import { environment } from "./environments/environment";
+
+if (environment.production) {
+  const FONT_SIZE = "font-size: 40px;";
+
+  console.log(
+    "%cFront%cend %cMentor",
+    FONT_SIZE + "color: #67bece;",
+    FONT_SIZE + "color: #3f54a3;",
+    FONT_SIZE +
+      "color: #fff;" +
+      "background-color: #3f54a3;" +
+      "border-radius: 8px;" +
+      "padding: 0 12px;",
+  );
+  console.log("Challenge by Frontend Mentor. Coded by Johnny Gérard.");
+  console.log("Code repository: https://github.com/johnnygerard/fem-order-summary-component");
+  console.log("Portfolio: https://www.frontendmentor.io/profile/johnnygerard");
+}
 
 bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err),
