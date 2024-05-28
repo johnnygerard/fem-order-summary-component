0dbb5e23a0b6fa3b70c85bdd19e92ce765b8018e
Johnny Gérard <john@jgerard.dev>
2024-05-29 01:34:34 +0200

    feat: add font links and config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["'Red Hat Display'", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
