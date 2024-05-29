3b91ba30174e5df1056e5fb77123270aea25a997
Johnny Gérard <john@jgerard.dev>
2024-05-29 19:41:14 +0200

    feat: add canvas color and background SVG

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,12 @@ export default {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      backgroundImage: {
+        curve: "url('/background.svg')",
+      },
+      colors: {
+        canvas: "#e0e8ff",
+      },
       fontFamily: {
         sans: ["'Red Hat Display'", ...defaultTheme.fontFamily.sans],
       },
